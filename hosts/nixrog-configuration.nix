a857a11aa6fbadcbd0588d42e511f72640caf825
Snekutaren <snekutaren@gmail.com>
2025-08-01 22:38:06 +0200

    Fri Aug  1 10:38:06 PM CEST 2025

--- hosts/nixrog-configuration.nix
+++ hosts/nixrog-configuration.nix
@@ -33,8 +33,8 @@
     baobab     # Best native choice
     kdePackages.filelight  # Optional: visual ring style
     qdirstat         # Optional: feature-rich tree map
-    agenix.inputs.${pkgs.system}.agenix # Agenix for secret managemen
-    #inputs.agenix.packages.${pkgs.system}.agenix # Agenix for secret management
+    #agenix.inputs.${pkgs.system}.agenix # Agenix for secret managemen
+    inputs.agenix.packages.${pkgs.system}.agenix # Agenix for secret management
     blueman       # bluetooth GUI management
     vulkan-tools  # For Vulkan support
     glxinfo       # For OpenGL information
