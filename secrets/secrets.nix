494d8b4e5748f58a46f74873e467f454e2d5029f
Snekutaren <snekutaren@gmail.com>
2025-08-03 00:22:22 +0200

    Sun Aug  3 12:22:22 AM CEST 2025

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 {
-
-  "owdious.age" = {
+  owdious = {
     file = ./owdious.age;
     owner = "owdious";
     publicKeys = [
@@ -8,7 +7,10 @@
     ];
   };
 
-  "restic.age".publicKeys = [
-    "age1dmkrdya4cckwfhm8vw3xlyke49lwcq8n69g58ccf5vxz26wh7p7quw0vzj"
-  ];
+  restic = {
+    file = ./restic.age;
+    publicKeys = [
+      "age1dmkrdya4cckwfhm8vw3xlyke49lwcq8n69g58ccf5vxz26wh7p7quw0vzj"
+    ];
+  };
 }
