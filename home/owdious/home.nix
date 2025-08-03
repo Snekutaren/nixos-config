8af9d43564aece9f99cb0d5178057510f0047c5f
Snekutaren <snekutaren@gmail.com>
2025-08-03 21:58:35 +0200

    Sun Aug  3 09:58:35 PM CEST 2025

--- home/owdious/home.nix
+++ home/owdious/home.nix
@@ -28,27 +28,26 @@
     # Example for zsh:
     # enableZshIntegration = true;
   };
+  
   # For the shell to hook into direnv, you need to enable it as a program.
   # This is often already present, but it's good to ensure it's there.
   programs.bash = {
-  enable = true;
-
+    enable = true;
     # Define aliases
     shellAliases = {
-      alias ll = "ls -lah";
-      alias lR = "ls -laR";
-      alias lRl = "ls -laR | less";
-      alias ssha = "eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github/github_ed25519";
-      alias git-push-nixos="git -C ~/nixos-config checkout auto && git -C ~/nixos-config add . && (git -C ~/nixos-config commit -m "$(date)" || true) && git -C ~/nixos-config push";
-      alias git-push-dot="git -C ~/dotfiles checkout auto && git -C ~/dotfiles add . && (git -C ~/dotfiles commit -m "$(date)" || true) && git -C ~/dotfiles push";
-      alias git-push-comfyui="git -C ~/comfyui checkout auto && git -C ~/comfyui add . && (git -C ~/comfyui commit -m "$(date)" || true) && git -C ~/comfyui push";
-      alias update-flake="sudo nix flake update --flake ~/nixos-config -v";
-      alias build-nix="sudo nixos-rebuild switch --flake ~/nixos-config -v";
-      alias push-build-nix="update-flake && git-push-nixos && git-push-dot && build-nix";
-      alias comfyui="nix develop ~/comfyui/nix";
-      alias reload-conf="source ~/.bashrc && echo "Bash configuration reloaded." && hyprctl reload && echo "Hyprland configuration reloaded."";
+      ll = "ls -lah";
+      lR = "ls -laR";
+      lRl = "ls -laR | less";
+      ssha = "eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github/github_ed25519";
+      git-push-nixos="git -C ~/nixos-config checkout auto && git -C ~/nixos-config add . && (git -C ~/nixos-config commit -m "$(date)" || true) && git -C ~/nixos-config push";
+      git-push-dot="git -C ~/dotfiles checkout auto && git -C ~/dotfiles add . && (git -C ~/dotfiles commit -m "$(date)" || true) && git -C ~/dotfiles push";
+      git-push-comfyui="git -C ~/comfyui checkout auto && git -C ~/comfyui add . && (git -C ~/comfyui commit -m "$(date)" || true) && git -C ~/comfyui push";
+      update-flake="sudo nix flake update --flake ~/nixos-config -v";
+      build-nix="sudo nixos-rebuild switch --flake ~/nixos-config -v";
+      push-build-nix="update-flake && git-push-nixos && git-push-dot && build-nix";
+      comfyui="nix develop ~/comfyui/nix";
+      reload-conf="source ~/.bashrc && echo "Bash configuration reloaded." && hyprctl reload && echo "Hyprland configuration reloaded."";
     };
-
     # Use extraInit for environment variables and functions
     extraInit = ''
       export PATH="$HOME/.local/bin:$PATH"
