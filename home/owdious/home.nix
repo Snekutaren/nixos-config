2bcb4767723a0c93380764a31f7f9c81eac3cd98
Snekutaren <snekutaren@gmail.com>
2025-08-03 14:24:03 +0200

    Sun Aug  3 02:24:03 PM CEST 2025

--- home/owdious/home.nix
+++ home/owdious/home.nix
@@ -33,27 +33,27 @@
   programs.bash = {
   enable = true;
 
-  # Define aliases
-  shellAliases = {
-    alias ll="ls -lah";
-    alias lR="ls -laR";
-    alias lRl="ls -laR | less";
-    alias ssha="eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github/github_ed25519";
-    alias git-push-nixos="git -C ~/nixos-config checkout auto && git -C ~/nixos-config add . && (git -C ~/nixos-config commit -m "$(date)" || true) && git -C ~/nixos-config push";
-    alias git-push-dot="git -C ~/dotfiles checkout auto && git -C ~/dotfiles add . && (git -C ~/dotfiles commit -m "$(date)" || true) && git -C ~/dotfiles push";
-    alias git-push-comfyui="git -C ~/comfyui checkout auto && git -C ~/comfyui add . && (git -C ~/comfyui commit -m "$(date)" || true) && git -C ~/comfyui push";
-    alias update-flake="sudo nix flake update --flake ~/nixos-config -v";
-    alias build-nix="sudo nixos-rebuild switch --flake ~/nixos-config -v";
-    alias push-build-nix="update-flake && git-push-nixos && git-push-dot && build-nix";
-    alias comfyui="nix develop ~/comfyui/nix";
-    alias reload-conf="source ~/.bashrc && echo "Bash configuration reloaded." && hyprctl reload && echo "Hyprland configuration reloaded."";
-  };
+    # Define aliases
+    shellAliases = {
+      alias ll="ls -lah";
+      alias lR="ls -laR";
+      alias lRl="ls -laR | less";
+      alias ssha="eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github/github_ed25519";
+      alias git-push-nixos="git -C ~/nixos-config checkout auto && git -C ~/nixos-config add . && (git -C ~/nixos-config commit -m "$(date)" || true) && git -C ~/nixos-config push";
+      alias git-push-dot="git -C ~/dotfiles checkout auto && git -C ~/dotfiles add . && (git -C ~/dotfiles commit -m "$(date)" || true) && git -C ~/dotfiles push";
+      alias git-push-comfyui="git -C ~/comfyui checkout auto && git -C ~/comfyui add . && (git -C ~/comfyui commit -m "$(date)" || true) && git -C ~/comfyui push";
+      alias update-flake="sudo nix flake update --flake ~/nixos-config -v";
+      alias build-nix="sudo nixos-rebuild switch --flake ~/nixos-config -v";
+      alias push-build-nix="update-flake && git-push-nixos && git-push-dot && build-nix";
+      alias comfyui="nix develop ~/comfyui/nix";
+      alias reload-conf="source ~/.bashrc && echo "Bash configuration reloaded." && hyprctl reload && echo "Hyprland configuration reloaded."";
+    };
 
-  # Use extraInit for environment variables and functions
-  extraInit = ''
-    export PATH="$HOME/.local/bin:$PATH"
-  '';
-};
+    # Use extraInit for environment variables and functions
+    extraInit = ''
+      export PATH="$HOME/.local/bin:$PATH"
+    '';
+  };
 
   # Enable a shell (e.g., Zsh)
   programs.zsh = {
