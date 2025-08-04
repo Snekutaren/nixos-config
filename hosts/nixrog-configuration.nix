ab5313e613de8c2da62c242d18a6b2c58926c153
Snekutaren <snekutaren@gmail.com>
2025-08-04 12:30:42 +0200

    Mon Aug  4 12:30:42 PM CEST 2025

--- hosts/nixrog-configuration.nix
+++ hosts/nixrog-configuration.nix
@@ -1,13 +1,56 @@
-{ config, pkgs, inputs, lib, ... }:
+{ config, pkgs, inputs, lib, modulesPath, ... }:
 {
   imports = [
-    ./nixrog-hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+    #./nixrog-hardware-configuration.nix
     ../modules/networking.nix
     ../modules/localization.nix
     ../modules/sound.nix
     ../modules/backup.nix
     ../modules/hyprland.nix
   ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" "cryptd" "cifs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = {
+    cryptroot = {
+      device = lib.mkForce "/dev/disk/by-label/NIXOS_LUKS";
+      allowDiscards = true;
+      # Uncomment if using a persistent keyfile managed by agenix
+      # keyFile = "/etc/luks-keys/cryptroot.key";
+    };
+  };
+  fileSystems."/" = {
+    device = lib.mkForce "/dev/disk/by-label/NIXOS_ROOT";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = lib.mkForce "/dev/disk/by-label/NIXOS_BOOT";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.xone.enable = true;
+  hardware.xpad-noone.enable = true;
+  hardware.amdgpu = {
+    amdvlk.enable = false;
+  };
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+    ];
+  };
+
+  nix.settings.extra-sandbox-paths = [ "/dev/kfd" "/dev/dri/renderD128" ];
 
   # Ensures fileSystems entries are generated for regular nixos-rebuild (non-destructive).
   disko.enableConfig = true;
