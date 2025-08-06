b89a9a3726539b003956fc8ad75ed74aed95a951
Snekutaren <snekutaren@gmail.com>
2025-08-06 08:49:50 +0200

    ons  6 aug 2025 08:49:50 CEST

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,5 @@
 let
   nixrogKey = "age13vfwxc02qv6yw4fchu7hjrpg3y6d5x72jxm5afujquqf8tncrv0sjw07af"; # From keygen output
 in {
-  "nixrog-tellus.age".publicKeys = [nixrogKey];
+  "nixrog-tellus.age".publicKeys = [ nixrogKey ];
 }
