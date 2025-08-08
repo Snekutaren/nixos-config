b277c9d61dfa3ce66cfec58afe17faa35a342104
Snekutaren <snekutaren@gmail.com>
2025-08-08 10:30:28 +0200

    fre  8 aug 2025 10:30:28 CEST

--- modules/hypr/hyprland.nix
+++ modules/hypr/hyprland.nix
@@ -44,16 +44,38 @@
     MOZ_ENABLE_WAYLAND = "1";
     QT_QPA_PLATFORM = "wayland";
     WLR_NO_HARDWARE_CURSORS = "0";
-    GTK_THEME = "Catppuccin-Mocha-Standard-Lavender-Dark";
-    QT_STYLE_OVERRIDE = "kvantum";
-    XCURSOR_THEME = "Catppuccin-Mocha-Dark-Cursors";
+    #GTK_THEME = "Catppuccin-Mocha-Standard-Lavender-Dark";
+    #QT_STYLE_OVERRIDE = "kvantum";
+    #XCURSOR_THEME = "Catppuccin-Mocha-Dark-Cursors";
     XCURSOR_SIZE = "24";
   };
 
-  # Auto-start Hyprland on TTY1 login
-  environment.loginShellInit = ''
-    if [ "$(tty)" = "/dev/tty1" ]; then
-      exec Hyprland
-    fi
-  '';
+  systemd.user.services.hyprland = {
+    description = "Hyprland Wayland Compositor";
+    after = [ "graphical.target" "dbus.service" ];
+    wants = [ "dbus.service" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "exec";
+      ExecStartPre = [
+        "/bin/sh -c 'until [ -d /home/owdious/.config ]; do sleep 0.5; done'"
+        "/bin/sh -c 'until [ -r /home/owdious/.config/hypr/hyprland.conf ]; do sleep 0.5; done'"
+        "/bin/sh -c 'until pgrep -x dbus-daemon > /dev/null; do sleep 0.5; done'"
+      ];
+      ExecStart = "${pkgs.hyprland}/bin/Hyprland";
+      Restart = "always";
+      RestartSec = "2";
+      WorkingDirectory = "/home/owdious";
+      Environment = [
+        "HOME=/home/owdious"
+        #"PATH=/run/current-system/sw/bin:/home/owdious/.nix-profile/bin:/usr/local/bin:/usr/bin:/bin"
+        "XDG_SESSION_TYPE=wayland"
+        "XDG_CONFIG_HOME=/home/owdious/.config"
+        "XDG_DATA_HOME=/home/owdious/.local/share"
+        "XDG_CACHE_HOME=/home/owdious/.cache"
+      ];
+      StandardOutput = "journal";
+      StandardError = "journal";
+   };
+  };
 }
