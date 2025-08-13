bf602f62c1bc710c0b496556e9e6667bf4f5b762
Snekutaren <snekutaren@gmail.com>
2025-08-13 13:35:13 +0200

    ons 13 aug 2025 13:35:13 CEST

--- machines/nixqemu/pre-install-cache.nix
+++ machines/nixqemu/pre-install-cache.nix
@@ -1,16 +1,71 @@
-# nixos-config/modules/pre-install-cache.nix
-{ config, pkgs, ... }:
-
+# /tmp/pre-install-flake/flake.nix
 {
-  nix.settings = {
-    substituters = [
-      "http://10.0.20.100:5000?priority=1"
-      "https://cache.nixos.org?priority=10"
-    ];
-    trusted-public-keys = [
-      "truenas-nix-cache:F3PBg47BlOWSyBJ/J4dQKHCupuWPBVHeFwnx59uzzi8="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    ];
-    experimental-features = [ "nix-command" "flakes" ];
+  description = "Minimal pre-install flake for cache settings";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    nixosConfigurations.pre-install = nixpkgs.lib.nixosSystem { # <-- This is the key
+      inherit system;
+      modules = [
+        # This is your configuration module
+        {
+          nix.settings = {
+            substituters = [
+              "http://10.0.20.100:5000?priority=1"
+              "https://cache.nixos.org?priority=10"
+            ];
+            trusted-public-keys = [
+              "truenas-nix-cache:F3PBg47BlOWSyBJ/J4dQKHCupuWPBVHeFwnx59uzzi8="
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            ];
+            experimental-features = [ "nix-command" "flakes" ];
+          };
+          # Add a state version to make it a valid NixOS config
+          system.stateVersion = "25.05";
+          # A minimal package to ensure the build completes
+          environment.systemPackages = with pkgs; [ git wget ];
+          boot = {
+            initrd = {
+              #availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" "uhci_hcd" "ehci_pci" "virtio_pci" "virtio_blk" ];
+              #kernelModules = [ "dm-snapshot" "cryptd" "cifs" ];
+              luks.devices.cryptroot = {
+                device = "/dev/disk/by-label/NIXOS_LUKS";
+                preLVM = true;
+                allowDiscards = true;
+                # keyFile = "/etc/luks-keys/cryptroot.key";
+              };
+            };
+            #kernelModules = [ "kvm-amd" ];
+            #kernelPackages = pkgs.linuxPackages_latest;
+          };
+          boot.loader.grub.enable = false;
+          boot.loader.systemd-boot.enable = false;
+          boot.loader.generic-extlinux-compatible.enable = false;
+          boot.loader.efi.canTouchEfiVariables = false;
+          boot.isContainer = true;
+          
+          #disko.enableConfig = true;
+          fileSystems."/" = {
+            device =  "/dev/disk/by-label/NIXOS_ROOT";
+            fsType = "ext4";
+          };
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-label/NIXOS_BOOT";
+            fsType = "vfat";
+            options = [ "fmask=0077" "dmask=0077" ];
+          };
+          users.users.root = {
+            password = "ostfralla";
+          };
+        }
+      ];
+    };
   };
 }
--- modules/disko-qemu.sh
+++ modules/disko-qemu.sh
@@ -4,10 +4,23 @@ set -euo pipefail
 USER=qemu
 KEY_HOME="/home/$USER/.config/age/"
 
+# 1. Manually set up the temporary nix.conf file.
+export NIX_CONF_DIR=/tmp/nix-conf
+mkdir -p $NIX_CONF_DIR
+cat <<EOF > $NIX_CONF_DIR/nix.conf
+substituters = http://10.0.20.100:5000?priority=1 https://cache.nixos.org?priority=10
+trusted-public-keys = truenas-nix-cache:F3PBg47BlOWSyBJ/J4dQKHCupuWPBVHeFwnx59uzzi8= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+experimental-features = nix-command flakes
+EOF
+
+# 2. Restart the nix-daemon.
+# This must be run with sudo for systemctl to work.
+sudo systemctl restart nix-daemon
+
 mkdir -p "/tmp/nixos-config"
 git clone -b auto https://github.com/snekutaren/nixos-config.git /tmp/nixos-config
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko#disko -- --mode disko /tmp/nixos-config/machines/nixqemu/nixqemu-disko.nix
+sudo -E nix --experimental-features "nix-command flakes" run github:nix-community/disko#disko -- --mode disko /tmp/nixos-config/machines/nixqemu/nixqemu-disko.nix
 
 #sudo cryptsetup luksAddKey /dev/disk/by-label/NIXOS_LUKS
 
@@ -20,7 +33,7 @@ else
   echo "/tmp/age.key not found!"
 fi
 
-sudo nixos-install --root "/mnt" --flake "/tmp/nixos-config#nixqemu" -v
+sudo -E nixos-install --root "/mnt" --flake "/tmp/nixos-config#nixqemu" -v
 
 echo "Copying age.key to $KEY_HOME .."
 
